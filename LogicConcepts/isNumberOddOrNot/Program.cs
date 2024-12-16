9ccd73bf0ef1effa99160e7e7c747e895c3398b6
moises ureña f <s_ha_king@hotmail.com>
2024-12-16 04:21:03 -0500

    complete exercise 01

---
+++ LogicConcepts/isNumberOddOrNot/Program.cs
@@ -0,0 +1,32 @@
+﻿
+var numberString = string.Empty;
+do
+{
+    Console.Write("Ingrese numero entero o escriba 'Salir' para salir: ");
+    numberString = Console.ReadLine();
+    if (numberString!.ToLower() == "salir")
+    {
+        continue;
+    }
+    var numberInt = 0;
+    if(int.TryParse(numberString, out numberInt))
+    {
+        if (numberInt % 2 == 0)
+        {
+            Console.WriteLine($"El número {numberInt}, es par.");
+        }
+        else
+        {
+            Console.WriteLine($"El número {numberInt}, es impar.");
+        }
+
+    }
+    else
+    {
+        Console.WriteLine($"Lo que ingresaste: {numberString}, no es un número entero.");
+    }
+
+} while (numberString!.ToLower() != "salir");
+Console.WriteLine("Game Over");
+
+ 
